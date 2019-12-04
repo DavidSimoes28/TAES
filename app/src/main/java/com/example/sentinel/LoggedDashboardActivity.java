227c941f6a707662a2272838bf6d6b72bb9a42e5
David Simões <2170978@my.ipleiria.pt>
2019-12-04 21:08:45 +0000

    Button share on tweeter complete TAES19GT-27

--- app/src/main/java/com/example/sentinel/LoggedDashboardActivity.java
+++ app/src/main/java/com/example/sentinel/LoggedDashboardActivity.java
@@ -4,10 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,11 +30,13 @@ public class LoggedDashboardActivity extends AppCompatActivity {
 
     private int hum;
     private int temp;
+    private String localization;
+    private String globalEvaluation;
     private TextView temperaturaField,humidadeField, globalField,dateField;
+    private Button btnTweet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_dashboard);
@@ -44,17 +49,13 @@ public class LoggedDashboardActivity extends AppCompatActivity {
         temperaturaField = findViewById(R.id.textViewTemperature);
         humidadeField = findViewById(R.id.textViewHumidadel);
         globalField = findViewById(R.id.textViewGloball);
-
+        btnTweet = findViewById(R.id.buttonShare);
         dateField = findViewById(R.id.textViewDatel);
-
 
         DatabaseReference databasereference = FirebaseDatabase.getInstance().getReference();
         databasereference.child("Sensores").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull final DataSnapshot dataSnapshot) {
-
-
-
 
                 List<String> ids = new LinkedList<String>();
 
@@ -77,7 +78,8 @@ public class LoggedDashboardActivity extends AppCompatActivity {
                             if (Objects.equals(areaSnapshot.child("localizacao").getValue(String.class), spin.getItemAtPosition(position).toString())){
                                  hum = Integer.parseInt(areaSnapshot.child("humidade").getValue().toString());
                                  temp = Integer.parseInt(areaSnapshot.child("temperatura").getValue().toString());
-                                 dateField.setText(areaSnapshot.child("data").getValue().toString());
+                                 dateField.setText("Data do registo: " + areaSnapshot.child("data").getValue().toString());
+                                 localization = areaSnapshot.child("localizacao").getValue(String.class);
                                 humidadeField.setText(hum + "%");
                                 temperaturaField.setText(temp + " ºC");
 
@@ -86,14 +88,17 @@ public class LoggedDashboardActivity extends AppCompatActivity {
                                     temperaturaField.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.circle_textview_red, null));
                                     globalField.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.circle_textview_red, null));
                                     globalField.setText("MAU");
+                                    globalEvaluation = "MAU";
                                 }else if((temp<=35 && temp>=19) && (hum>=50 && hum<=75)){
                                     humidadeField.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.circle_textview_green, null));
                                     temperaturaField.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.circle_textview_green, null));
                                     globalField.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.circle_textview_green, null));
                                     globalField.setText("BOM");
+                                    globalEvaluation = "BOM";
                                 }else{
                                     globalField.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.circle_textview_yellow, null));
                                     globalField.setText("MÉDIO");
+                                    globalEvaluation = "MÉDIO";
                                     if(temp<19 || temp>35){
                                         temperaturaField.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.circle_textview_red, null));
 
@@ -128,6 +133,20 @@ public class LoggedDashboardActivity extends AppCompatActivity {
 
         });
 
+        btnTweet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ShareMessage = "Edificio: A\nLocalização:"+ localization
+                        + "\nTemperatura: "+ temp
+                        + "ºC\nHumidade: "+ hum
+                        + "\nAvaliação geral: " + globalEvaluation
+                        + "\n\n Sentinel Application";
+                String tweetUrl = "https://twitter.com/intent/tweet?text=" + ShareMessage;
+                Uri uri = Uri.parse(tweetUrl);
+                startActivity(new Intent(Intent.ACTION_VIEW, uri));
+                //https://stackoverflow.com/questions/14664763/android-sharing-on-twitter
+            }
+        });
         }
     }
 
